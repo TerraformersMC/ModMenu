4c6aaecc36a71e8f736b1155a44fbd44e1e5b5a2
Prospector <prospectordev@gmail.com>
2019-05-13 20:36:03 -0700

    Use fabric 0.3.0, Include fabric-resource-loader-v0, and Fix realms icons rendering at the wrong spot.

--- src/main/java/io/github/prospector/modmenu/mixin/MainMenuScreenMixin.java
+++ src/main/java/io/github/prospector/modmenu/mixin/MainMenuScreenMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.network.chat.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MainMenuScreen.class)
@@ -25,6 +26,11 @@ public class MainMenuScreenMixin extends Screen {
 		int i = FabricLoader.getInstance().getAllMods().size();
 		this.addButton(new ModMenuButtonWidget(this.width / 2 - 100, this.height / 4 + 48 + 24 * 3, 200, 20, (ModMenu.noFabric ? "Mods" : I18n.translate("modmenu.title")) + " " + (
 			ModMenu.noFabric ? "(" + i + " Loaded)" : I18n.translate("modmenu.loaded", i)), this));
+	}
+
+	@ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Screen;init(Lnet/minecraft/client/MinecraftClient;II)V"), method = "init", index = 2)
+	private int adjustRealmsHeight(int height) {
+		return height - 51;
 	}
 
 	protected <T extends AbstractButtonWidget> T addButton(T button) {
