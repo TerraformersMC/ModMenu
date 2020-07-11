2b9405467fc5da981b6932c83082e2ecd68870e8
Yanis48 <doublecraft.official@gmail.com>
2020-07-11 16:39:39 +0200

    updated colors

--- src/main/java/io/github/prospector/modmenu/util/BadgeType.java
+++ src/main/java/io/github/prospector/modmenu/util/BadgeType.java
@@ -4,11 +4,11 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
 public enum BadgeType {
-	LIBRARY(new TranslatableText("modmenu.library"), 0x8810d098, 0x88046146),
-	CLIENTSIDE(new TranslatableText("modmenu.clientsideOnly"), 0x884383E3, 0x880E4699),
-	DEPRECATED(new TranslatableText("modmenu.deprecated"), 0x88C10A0B, 0x88E8393B),
-	PATCHWORK_FORGE(new TranslatableText("modmenu.forge"), 0x887C89A3, 0x88202C43),
-	MINECRAFT(new TranslatableText("modmenu.minecraft"), 0x88BCBCBC, 0x88535353);
+	LIBRARY(new TranslatableText("modmenu.library"), 0xff107454, 0xff093929),
+	CLIENTSIDE(new TranslatableText("modmenu.clientsideOnly"), 0xff2b4b7c, 0xff0e2a55),
+	DEPRECATED(new TranslatableText("modmenu.deprecated"), 0xffff3333, 0xffb30000),
+	PATCHWORK_FORGE(new TranslatableText("modmenu.forge"), 0xff1f2d42, 0xff101721),
+	MINECRAFT(new TranslatableText("modmenu.minecraft"), 0xff6f6c6a, 0xff31302f);
 	
 	private Text text;
 	private int outlineColor;
