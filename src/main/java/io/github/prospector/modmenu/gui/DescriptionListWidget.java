86e3bec95fe8788fc1e767e9f200b4b0607df026
Prospector <prospectordev@gmail.com>
2020-11-10 20:57:42 -0800

    Update mappings, move to Terraformers build stuff
    Took 24 seconds

--- src/main/java/io/github/prospector/modmenu/gui/DescriptionListWidget.java
+++ src/main/java/io/github/prospector/modmenu/gui/DescriptionListWidget.java
@@ -8,7 +8,6 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.widget.EntryListWidget;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
