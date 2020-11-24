21e041501a76ca74b04014a7289ac0fa9b53fe2e
Prospector <prospectordev@gmail.com>
2020-11-23 19:23:00 -0800

    #change "Backport a fix for the config file not loading properly to 1.14.4" #change "Backport support for Patchwork's new metadata system to 1.14.4" #release

--- src/main/java/io/github/prospector/modmenu/gui/ModListWidget.java
+++ src/main/java/io/github/prospector/modmenu/gui/ModListWidget.java
@@ -7,7 +7,6 @@ import io.github.prospector.modmenu.gui.entries.ChildEntry;
 import io.github.prospector.modmenu.gui.entries.IndependentEntry;
 import io.github.prospector.modmenu.gui.entries.ParentEntry;
 import io.github.prospector.modmenu.util.HardcodedUtil;
-import io.github.prospector.modmenu.util.TestModContainer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
@@ -125,7 +124,7 @@ public class ModListWidget extends AlwaysSelectedEntryListWidget<ModListEntry> i
 
 		if (DEBUG) {
 			mods = new ArrayList<>(mods);
-			mods.addAll(TestModContainer.getTestModContainers());
+//			mods.addAll(TestModContainer.getTestModContainers());
 		}
 
 		if (this.modContainerList == null || refresh) {
