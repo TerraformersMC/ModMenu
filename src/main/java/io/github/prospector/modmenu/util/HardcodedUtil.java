672c9be7e17c6ff1aff5568d0dd1a9d047ef5b2b
williambl <william@williambl.com>
2021-01-17 13:40:31 +0000

    Move hardcoded icons into HardcodedUtil

--- src/main/java/io/github/prospector/modmenu/util/HardcodedUtil.java
+++ src/main/java/io/github/prospector/modmenu/util/HardcodedUtil.java
@@ -8,10 +8,13 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.apache.commons.lang3.text.WordUtils;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,11 +22,16 @@ public class HardcodedUtil {
 	private static final Pattern FABRIC_PATTERN = Pattern.compile("^fabric-.*(-v\\d+)$");
 	private static final Set<String> FABRIC_MODS = new HashSet<>();
 	private static final HashMap<String, String> HARDCODED_DESCRIPTIONS = new HashMap<>();
+	private static final HashMap<Predicate<ModMetadata>, Path> HARDCODED_ICONS = new HashMap<>();
 
 	public static void initializeHardcodings() {
 		FABRIC_MODS.add("fabric");
 		FABRIC_MODS.add("fabricloader");
 		HARDCODED_DESCRIPTIONS.put("minecraft", "The base game.");
+		HARDCODED_DESCRIPTIONS.put("java", "The Java Runtime Environment");
+		ModContainer modMenu = FabricLoader.getInstance().getModContainer(ModMenu.MOD_ID).orElseThrow(IllegalAccessError::new);
+		HARDCODED_ICONS.put(mod -> getFabricMods().contains(mod.getId()), modMenu.getPath("assets/" + ModMenu.MOD_ID + "/fabric_icon.png"));
+		HARDCODED_ICONS.put(mod -> mod.getId().equals("minecraft"), modMenu.getPath("assets/" + ModMenu.MOD_ID + "/mc_icon.png"));
 	}
 
 	public static void hardcodeModuleMetadata(ModContainer mod, ModMetadata metadata, String id) {
@@ -58,6 +66,18 @@ public class HardcodedUtil {
 			name = name.replace("Blockentity", "BlockEntity");
 		}
 		return new LiteralText(name);
+	}
+
+	public static boolean modHasHardcodedIcon(ModMetadata mod) {
+		return HARDCODED_ICONS.keySet().stream().anyMatch(predicate -> predicate.test(mod));
+	}
+
+	public static Path getHardcodedIcon(ModMetadata mod) {
+		AtomicReference<Path> result = new AtomicReference<>();
+		HARDCODED_ICONS.forEach((predicate, path) -> {
+			if (predicate.test(mod)) result.set(path);
+		});
+		return result.get();
 	}
 
 	public static String getHardcodedDescription(String id) {
