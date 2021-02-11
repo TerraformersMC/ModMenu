a24f6ae4461718bc3b7aef3a9b1a76ae8b867fb7
Prospector <prospectordev@gmail.com>
2021-02-10 17:36:33 -0800

    21w06a
    Took 8 minutes

--- src/main/java/io/github/prospector/modmenu/VanillaModMenuCompat.java
+++ src/main/java/io/github/prospector/modmenu/VanillaModMenuCompat.java
@@ -4,7 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.options.OptionsScreen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
 
 import java.util.Map;
 
