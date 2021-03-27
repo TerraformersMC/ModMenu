d83af37ae520810bd14b537a2e038bfb56f34d98
Joe van der Zwet <me@joezwet.me>
2021-03-28 12:06:54 +1300

    chore: remove unused imports

--- src/main/java/com/terraformersmc/modmenu/updates/providers/GithubUpdateProvider.java
+++ src/main/java/com/terraformersmc/modmenu/updates/providers/GithubUpdateProvider.java
@@ -10,8 +10,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
