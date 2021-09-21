e790f15259082e49d79db69a8073359dc4cdc31a
jackassmc <84429870+jackassmc@users.noreply.github.com>
2021-09-21 04:15:42 +0200

    Fix crash caused by broken mod config screens
    - Fixed Minecraft crash caused by broken mod config screens

--- src/main/java/com/terraformersmc/modmenu/gui/ModsScreen.java
+++ src/main/java/com/terraformersmc/modmenu/gui/ModsScreen.java
@@ -124,6 +124,7 @@ public class ModsScreen extends Screen {
 					modHasConfigScreen.put(mod.getId(), false);
 				} catch (Throwable e) {
 					LOGGER.error("Error from mod '" + mod.getId() + "'", e);
+					modHasConfigScreen.put(mod.getId(), false);
 				}
 			}
 		}
