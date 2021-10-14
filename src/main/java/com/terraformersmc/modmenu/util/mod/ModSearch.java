83bd2b3b15b6cb74f0ebfd5766adac860e43e5c0
Madis <Madis0@users.noreply.github.com>
2021-10-14 20:17:52 +0300

    Add description and summary search

--- src/main/java/com/terraformersmc/modmenu/util/mod/ModSearch.java
+++ src/main/java/com/terraformersmc/modmenu/util/mod/ModSearch.java
@@ -24,10 +24,14 @@ public class ModSearch {
 
 	private static boolean passesFilters(ModsScreen screen, Mod mod, String query) {
 		String modId = mod.getId();
+		String modDescription = mod.getDescription();
+		String modSummary = mod.getSummary();
 
 		// Some basic search, could do with something more advanced but this will do for now
 		if (mod.getName().toLowerCase(Locale.ROOT).contains(query) // Search mod name
 				|| modId.toLowerCase(Locale.ROOT).contains(query) // Search mod name
+				|| modDescription.toLowerCase(Locale.ROOT).contains(query) // Search mod name
+				|| modSummary.toLowerCase(Locale.ROOT).contains(query) // Search mod name
 				|| authorMatches(mod, query) // Search via author
 				|| (mod.getBadges().contains(Mod.Badge.LIBRARY) && "api library".contains(query)) // Search for lib mods
 				|| ("patchwork forge".contains(query) && mod.getBadges().contains(Mod.Badge.PATCHWORK_FORGE)) // Search for patchwork mods
