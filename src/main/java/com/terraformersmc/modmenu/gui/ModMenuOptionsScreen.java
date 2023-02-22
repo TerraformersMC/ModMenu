c850ec696b8934f0202f5f5b139e2defc53d5a4f
Cat Core <arthur.koalas@free.fr>
2023-02-22 17:31:18 +0100

    Port to 23w07a, Mappings changes only

--- src/main/java/com/terraformersmc/modmenu/gui/ModMenuOptionsScreen.java
+++ src/main/java/com/terraformersmc/modmenu/gui/ModMenuOptionsScreen.java
@@ -6,8 +6,7 @@ import com.terraformersmc.modmenu.util.compat.MCCompat;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
-import net.minecraft.client.gui.widget.ButtonListWidget;
-import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.OptionListWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
@@ -15,7 +14,7 @@ import net.minecraft.text.Text;
 public class ModMenuOptionsScreen extends GameOptionsScreen {
 
 	private Screen previous;
-	private ButtonListWidget list;
+	private OptionListWidget list;
 
 	@SuppressWarnings("resource")
 	public ModMenuOptionsScreen(Screen previous) {
@@ -25,7 +24,7 @@ public class ModMenuOptionsScreen extends GameOptionsScreen {
 
 
 	protected void init() {
-		this.list = new ButtonListWidget(this.client, this.width, this.height, 32, this.height - 32, 25);
+		this.list = new OptionListWidget(this.client, this.width, this.height, 32, this.height - 32, 25);
 		this.list.addAll(ModMenuConfig.asOptions());
 		this.addSelectableChild(this.list);
 		this.addDrawableChild(
@@ -43,13 +42,13 @@ public class ModMenuOptionsScreen extends GameOptionsScreen {
 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
 		this.renderBackground(matrices);
 		this.list.render(matrices, mouseX, mouseY, delta);
-		drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 5, 0xffffff);
+		drawCenteredTextWithShadow(matrices, this.textRenderer, this.title, this.width / 2, 5, 0xffffff);
 		super.render(matrices, mouseX, mouseY, delta);
 
 		MCCompat.getInstance().getTooltipHelper().modMenuOptionsScreen$render(this, matrices, mouseX, mouseY);
 	}
 
-	public ButtonListWidget getList() {
+	public Object getList() {
 		return list;
 	}
 
