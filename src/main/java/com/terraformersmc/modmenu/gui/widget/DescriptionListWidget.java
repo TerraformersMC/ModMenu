aa4b10ab6f4333b1611eb80b9e7e00427fc77671
Rashnain <51191602+Rashnain@users.noreply.github.com>
2023-07-17 14:53:23 +0200

    display mod description texts below shadows

--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -161,6 +161,10 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 			tessellator.draw();
 		}
 
+		this.enableScissor(DrawContext);
+		this.renderList(DrawContext, mouseX, mouseY, delta);
+		DrawContext.disableScissor();
+
 		RenderSystem.depthFunc(515);
 		RenderSystem.disableDepthTest();
 		RenderSystem.enableBlend();
@@ -209,10 +213,6 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 
 				next();
 		tessellator.draw();
-
-		this.enableScissor(DrawContext);
-		this.renderList(DrawContext, mouseX, mouseY, delta);
-		DrawContext.disableScissor();
 
 		this.renderScrollBar(bufferBuilder, tessellator);
 
