42742dbda309902eb4c7606427c7535a744bca7f
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-06-02 00:16:08 -0400

    Update src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
    Co-authored-by: haykam821 <24855774+haykam821@users.noreply.github.com>

--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -345,8 +345,9 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 				builtBuffer = bufferBuilder.end();
 				BufferRenderer.drawWithGlobalProgram(builtBuffer);
 				builtBuffer.close();
+			} catch (Exception e) {
+				// Ignored
 			}
-			catch (Exception ignored) { }
 		}
 	}
 
