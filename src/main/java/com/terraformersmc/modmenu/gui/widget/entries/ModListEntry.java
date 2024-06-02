365bfd66bbfbcf942339ed75f4f877fe1cc9219a
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-06-02 00:15:16 -0400

    Update src/main/java/com/terraformersmc/modmenu/gui/widget/entries/ModListEntry.java
    Co-authored-by: haykam821 <24855774+haykam821@users.noreply.github.com>

--- src/main/java/com/terraformersmc/modmenu/gui/widget/entries/ModListEntry.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/entries/ModListEntry.java
@@ -21,10 +21,10 @@ import net.minecraft.util.Language;
 import net.minecraft.util.Util;
 
 public class ModListEntry extends AlwaysSelectedEntryListWidget.Entry<ModListEntry> {
-	public static final Identifier UNKNOWN_ICON = Identifier.of("textures/misc/unknown_pack.png");
+	public static final Identifier UNKNOWN_ICON = Identifier.ofVanilla("textures/misc/unknown_pack.png");
 	private static final Identifier MOD_CONFIGURATION_ICON = Identifier.of(ModMenu.MOD_ID, "textures/gui/mod_configuration.png");
-	private static final Identifier ERROR_ICON = Identifier.of("world_list/error");
-	private static final Identifier ERROR_HIGHLIGHTED_ICON = Identifier.of("world_list/error_highlighted");
+	private static final Identifier ERROR_ICON = Identifier.ofVanilla("world_list/error");
+	private static final Identifier ERROR_HIGHLIGHTED_ICON = Identifier.ofVanilla("world_list/error_highlighted");
 
 	protected final MinecraftClient client;
 	public final Mod mod;
