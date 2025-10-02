3a9d204664c65f7ca4a57b0ad3f6f3ccccdf63e8
Sakura Ryoko <sakura@ryoko.soy>
2025-10-02 11:24:53 -0400

    notate fix: move "update" icon to match the "Realms" icon's position. (Issue # 769)

--- src/main/java/com/terraformersmc/modmenu/gui/widget/ModMenuButtonWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/ModMenuButtonWidget.java
@@ -28,7 +28,8 @@ public class ModMenuButtonWidget extends ButtonWidget {
 		if (ModMenuConfig.BUTTON_UPDATE_BADGE.getValue() && ModMenu.areModUpdatesAvailable()) {
 			UpdateAvailableBadge.renderBadge(
 				drawContext,
-				this.width + this.getX() - 16,
+				this.width + this.getX() - 16,      // 1.21.8
+//				this.width + this.getX() - 13,      // Matches Realms Position
 				this.height / 2 + this.getY() - 4
 			);
 		}
