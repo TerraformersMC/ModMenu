d8d999965483b21bd30b86e9a02c44477453717d
Jab125 <67534807+Jab125@users.noreply.github.com>
2026-05-25 22:53:35 +1000

    Update UpdateAvailableBadge.java

--- src/main/java/com/terraformersmc/modmenu/gui/widget/UpdateAvailableBadge.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/UpdateAvailableBadge.java
@@ -9,7 +9,7 @@ public class UpdateAvailableBadge {
     private static final Identifier UPDATE_ICON = Identifier.withDefaultNamespace("icon/trial_available");
 
     public static void renderBadge(GuiGraphicsExtractor drawContext, int x, int y) {
-        drawContext.blitSprite(RenderPipelines.GUI_TEXTURED, UPDATE_ICON, x, y, 8, 8, 1.0F);
+        renderBadge(drawContext, x, y, 1.0F);
     }
 
     public static void renderBadge(GuiGraphicsExtractor drawContext, int x, int y, float alpha) {
